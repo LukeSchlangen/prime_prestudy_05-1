77ca6f1cac0f612e1f22e764d68f964e3d5b1c8c
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-01-25 18:15:57 -0600

    changed JS

--- script.js
+++ script.js
@@ -8,3 +8,13 @@
 //	releaseYear = 2012;
 //	"Director" : "Joss Whedon".
 //}
+
+
+var avengersMovie = {
+  name: "Avengers", // added "" around avangers  // changed ; to a ,
+  runtime: 143 + " mins",  // removed mins tag  // changed ; to a ,  //  added + "" around mins
+  releaseYear: 2012,  // replaced the = with an :  // changed ; to a ,
+  director: "Joss Whedon",  // added :  // changed . to a ,
+};
+
+console.log(avengersMovie);
